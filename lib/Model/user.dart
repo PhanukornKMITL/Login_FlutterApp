9a8dbc0484640e1d07e5e5a83989a4d5c5b79867
Bank <60050230@kmitl.ac.th>
2021-05-09 15:19:16 +0700

    ini firebase

---
+++ lib/Model/user.dart
@@ -0,0 +1,13 @@
+class User{
+  String _username;
+  String _password;
+
+  String get username{
+    return _username;
+  }
+
+  set username(String username){
+    _username = username;
+  }
+
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:login_app/Register/register_page.dart';
 import 'package:login_app/login_page.dart';
+import 'package:firebase_core/firebase_core.dart';
 
 import 'Profile/profile.dart';
 
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(App());
 }
 
